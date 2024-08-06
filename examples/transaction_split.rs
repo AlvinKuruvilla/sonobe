a87f349c35237558211777f369138c6edefa7e67
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2024-08-05 20:39:29 -0400

    validate all leaf elements

--- examples/transaction_split.rs
+++ examples/transaction_split.rs
@@ -3,19 +3,24 @@ use ark_r1cs_std::{alloc::AllocVar, fields::fp::FpVar};
 use ark_relations::r1cs::ConstraintSystem;
 use folding_schemes::zcash::merkle_gadget::MerkleTreeGadget;
 fn main() {
-    // TODO: Test with splitting logic
     let cs = ConstraintSystem::<Fr>::new_ref();
 
     // Create leaves
-    let leaf_values: Vec<Fr> = vec![10, 20, 30, 40].into_iter().map(Fr::from).collect();
+    // NOTE: the original vector was [10, 20, 15, 15, 40], the "30" element was manually split to test if we can
+    // validate all elements in a set of leaves
+    // We may need to further test this ability but will need to consult notes for that
+    let leaf_values: Vec<Fr> = vec![10, 20, 15, 15, 40].into_iter().map(Fr::from).collect();
 
     // Convert leaves to FpVar
     let leaves_fpvar: Vec<_> = leaf_values
         .iter()
         .map(|&leaf| FpVar::new_input(cs.clone(), || Ok(leaf)).unwrap())
         .collect();
-    // Verify the chosen leaf
-    let is_valid = MerkleTreeGadget::generate_proof_and_validate(&leaves_fpvar, cs, vec![1]);
+    for (idx, _) in leaves_fpvar.iter().enumerate() {
+        // Verify the chosen leaf
+        let is_valid =
+            MerkleTreeGadget::generate_proof_and_validate(&leaves_fpvar, cs.clone(), vec![idx]);
 
-    println!("Leaf node is valid: {}", is_valid);
+        println!("Leaf node is valid: {}", is_valid);
+    }
 }
